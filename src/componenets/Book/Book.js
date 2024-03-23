7efd0a7055050ee1a14d96f39ed299b4e575271b
Ajay Hrishi V <ajayhrishi@gmail.com>
2024-03-24 03:12:00 +0530

    hosting front end

--- src/componenets/Book/Book.js
+++ src/componenets/Book/Book.js
@@ -2,44 +2,30 @@ import React from 'react';
 import './bookStyle.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-const Book = ({book}) => {
-//   if (!props.book) {
-//     // Handle the case when props.book is undefined
-//     return null; // You can return a placeholder, message, or null as appropriate
-//   }
+
+const Book = ({ book, onDelete }) => {
   const history = useNavigate();
-  const {
-    _id,
-    name,
-    author,
-    price,
-    description,
-    available,
-    image
-  } = book;
 
-  const UpdateHandler = ()=>{
+  const { _id, name, author, price, description, available, image } = book;
+
+  const handleUpdate = () => {
     history(`/UpdateBook/${_id}`);
-  }
-
-  const DeleteHandler = async() => {
-    await axios.delete(`https://book-store-backend-n1sr.onrender.com/deleteById/${_id}`).then(res=>res.data).then(()=>history("/AllBooks"));
-  }
+  };
 
   return (
     <div className="book-container">
-    <img className="book-image" src={image} alt={book.title} />
-    <div className="book-title">{name}</div>
-    <div className="book-author">{author}</div>
-    <div className="book-price">${price}</div>
-    <p className="book-description">{description}</p>
-    <p className="book-description">{available}</p>
-    <div className='bookButtonBox'>
-    <button onClick={UpdateHandler}  className="book-button-update"> Update Book </button>
-    <button onClick={DeleteHandler} className="book-button-delete">Delete Books</button>
+      <img className="book-image" src={image} alt={book.title} />
+      <div className="book-title">{name}</div>
+      <div className="book-author">{author}</div>
+      <div className="book-price">${price}</div>
+      <p className="book-description">{description}</p>
+      <p className="book-description">{available}</p>
+      <div className='bookButtonBox'>
+        <button onClick={handleUpdate} className="book-button-update"> Update Book </button>
+        <button onClick={() => onDelete(_id)} className="book-button-delete">Delete Book</button>
+      </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Book;
--- src/componenets/Book/Books.js
+++ src/componenets/Book/Books.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Book from './Book';
 import axios from 'axios';
 import './bookStyle.css';
+import { useNavigate } from 'react-router-dom';
 
 const Books = () => {
+  const navigate = useNavigate();
   const URL = 'https://book-store-backend-n1sr.onrender.com/AllBooks';
   const [books, setBooks] = useState([]);
 
@@ -16,18 +18,26 @@ const Books = () => {
     }
   };
 
+  const handleDeleteBook = async (bookId) => {
+    try {
+      await axios.delete(`https://book-store-backend-n1sr.onrender.com/deleteById/${bookId}`).then(()=>{navigate('/AllBooks')});
+      // After successful deletion, fetch all books again to update the books state
+      getAllBooks();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     getAllBooks(); // Call the function directly
-  }, [books]);
-
-  console.log(books);
+  }, []);
 
   return (
     <div>
       <ul className='bookshelf'>
         {books.map((book, i) => (
           <div key={i}>
-            <Book book={book} />
+            <Book book={book} onDelete={() => handleDeleteBook(book._id)} />
           </div>
         ))}
       </ul>
